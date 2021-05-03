9afec76616e0fdece44225c976fe77ac9e0d36b3
Hartmut Holzgraefe <hartmut@php.net>
2021-05-04 01:43:58 +0200

    long overdue change from depreacted "mysql" extension to "mysqli"

--- change_definer.php
+++ change_definer.php
@@ -5,7 +5,7 @@
 $for_real = true; // execute DDL statements
 $verbose  = true; // show DDL statements 
 
-$db_host = "127.0.0.1";
+$db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
 
@@ -26,48 +26,44 @@ $old_definer=$old_definer_user."@".$old_definer_host;
 
 $new_definer_sql = "`$new_definer_user`@`$new_definer_host`";
 
-
-function ddl($query)
-{
-  global $verbose, $for_real;
-
-  if ($verbose) {
-    echo "Query: $query\n\n";
-  }
-  if ($for_real) {
-    mysql_query($query) or die(mysql_error());
-  }
+$conn = mysqli_init();
+if (!$conn) {
+    die('mysqli_init failed');
 }
 
-mysql_connect($db_host, $db_user, $db_pass) or die("can't connect to mysql");
+if (!mysqli_real_connect($conn, $db_host, $db_user, $db_pass)) {
+    die('Connect Error (' . mysqli_connect_errno() . ') '
+        . mysqli_connect_error());
+}
 
 // modify views
 
 $get_views = "SELECT * FROM INFORMATION_SCHEMA.VIEWS WHERE DEFINER = '$old_definer'";
 
-$res = mysql_query($get_views) or die(mysql_error());
-while($row = mysql_fetch_assoc($res)) {
+$res = mysqli_query($conn, $get_views) or die(mysqli_error($conn));
+
+while($row = mysqli_fetch_assoc($res)) {
   echo "modifying view $row[TABLE_SCHEMA].$row[TABLE_NAME]\n";
 
-  mysql_select_db($row["TABLE_SCHEMA"]);
+  mysqli_select_db($conn, $row["TABLE_SCHEMA"]);
 
   $set = "SET SESSION character_set_client='$row[CHARACTER_SET_CLIENT]', collation_connection='$row[COLLATION_CONNECTION]'";
   $alter = "ALTER DEFINER=$new_definer_sql VIEW `$row[TABLE_NAME]` AS $row[VIEW_DEFINITION]";
   
-  ddl($set);
-  ddl($alter);
+  ddl($conn, $set);
+  ddl($conn, $alter);
 }
-mysql_free_result($res);
+mysqli_free_result($res);
 
 // modify triggers
 
 $get_triggers = "SELECT * FROM INFORMATION_SCHEMA.TRIGGERS WHERE DEFINER = '$old_definer'";
 
-$res = mysql_query($get_triggers) or die(mysql_error());
-while($row = mysql_fetch_assoc($res)) {
+$res = mysqli_query($conn, $get_triggers) or die(mysqli_error($conn));
+while($row = mysqli_fetch_assoc($res)) {
   echo "modifying trigger $row[TRIGGER_SCHEMA].$row[TRIGGER_NAME] on table $row[EVENT_OBJECT_SCHEMA].$row[EVENT_OBJECT_TABLE]\n";
 
-  mysql_select_db($row["TRIGGER_SCHEMA"]);
+  mysqli_select_db($conn, $row["TRIGGER_SCHEMA"]);
 
   $lock = "LOCK TABLES `$row[EVENT_OBJECT_TABLE]` WRITE";
   $unlock = "UNLOCK TABLES";
@@ -76,29 +72,29 @@ while($row = mysql_fetch_assoc($res)) {
 
   $show = "SHOW CREATE TRIGGER `$row[TRIGGER_NAME]`";
 
-  $res2 = mysql_query($show) or die($show.":".mysql_error());
-  $row2 = mysql_fetch_assoc($res2);
-  mysql_free_result($res2);
+  $res2 = mysqli_query($conn, $show) or die($show.":".mysqli_error($conn));
+  $row2 = mysqli_fetch_assoc($res2);
+  mysqli_free_result($res2);
 
   $set = "SET SESSION character_set_client='$row[CHARACTER_SET_CLIENT]', collation_connection='$row[COLLATION_CONNECTION]'";
   $sql_mode = "SET SESSION SQL_MODE = '$row2[sql_mode]'";
 
   $create = preg_replace('|^CREATE DEFINER=('.preg_quote($old_definer_sql).')|', "CREATE DEFINER=$new_definer_sql", $row2["SQL Original Statement"]);
 
-  ddl($set);
-  ddl($sql_mode);
-  ddl($lock);
-  ddl($drop);
-  ddl($create);
-  ddl($unlock);
+  ddl($conn, $set);
+  ddl($conn, $sql_mode);
+  ddl($conn, $lock);
+  ddl($conn, $drop);
+  ddl($conn, $create);
+  ddl($conn, $unlock);
 }  
 
 // modify events
 
 $get_events = "SELECT * FROM INFORMATION_SCHEMA.EVENTS WHERE DEFINER = '$old_definer'";
 
-$res = mysql_query($get_events) or die(mysql_error());
-while($row = mysql_fetch_assoc($res)) {
+$res = mysqli_query($conn, $get_events) or die(mysqli_error($conn));
+while($row = mysqli_fetch_assoc($res)) {
   echo "modifying event $row[EVENT_SCHEMA].$row[EVENT_NAME]\n";
 
   $set = "SET SESSION character_set_client='$row[CHARACTER_SET_CLIENT]', collation_connection='$row[COLLATION_CONNECTION]'";
@@ -106,27 +102,27 @@ while($row = mysql_fetch_assoc($res)) {
   // one extra option is needed after EVENT, ON COMPLETION is the simplest so we take this
   $alter = "ALTER DEFINER=$new_definer_sql EVENT `$row[EVENT_SCHEMA]`.`$row[EVENT_NAME]` ON COMPLETION $row[ON_COMPLETION]";
 
-  ddl($set);
-  ddl($alter);
+  ddl($conn, $set);
+  ddl($conn, $alter);
 }
 
 // modify procedures and functions
 
 $get_procedures = "SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE DEFINER = '$old_definer'";
 
-$res = mysql_query($get_procedures) or die(mysql_error());
-while($row = mysql_fetch_assoc($res)) {
+$res = mysqli_query($conn, $get_procedures) or die(mysqli_error($conn));
+while($row = mysqli_fetch_assoc($res)) {
   echo "modifying ".strtolower($row['ROUTINE_TYPE'])." $row[ROUTINE_SCHEMA].$row[ROUTINE_NAME]\n";
 
-  mysql_select_db($row["ROUTINE_SCHEMA"]);
+  mysqli_select_db($conn, $row["ROUTINE_SCHEMA"]);
 
   $drop = "DROP $row[ROUTINE_TYPE] `$row[ROUTINE_NAME]`";
 
   $show = "SHOW CREATE $row[ROUTINE_TYPE] `$row[ROUTINE_NAME]`";
 
-  $res2 = mysql_query($show) or die($show.":".mysql_error());
-  $row2 = mysql_fetch_assoc($res2);
-  mysql_free_result($res2);
+  $res2 = mysqli_query($conn, $show) or die($show.":".mysqli_error($conn));
+  $row2 = mysqli_fetch_assoc($res2);
+  mysqli_free_result($res2);
 
   $set = "SET SESSION character_set_client='$row[CHARACTER_SET_CLIENT]', collation_connection='$row[COLLATION_CONNECTION]'";
 
@@ -134,8 +130,22 @@ while($row = mysql_fetch_assoc($res)) {
 
   $create = preg_replace('|^CREATE DEFINER=('.preg_quote($old_definer_sql).')|', "CREATE DEFINER=$new_definer_sql", $row2["Create ".ucwords(strtolower($row['ROUTINE_TYPE']))]);
 
-  ddl($set);
-  ddl($sql_mode);
-  ddl($drop);
-  ddl($create);
+  ddl($conn, $set);
+  ddl($conn, $sql_mode);
+  ddl($conn, $drop);
+  ddl($conn, $create);
 }
+
+
+function ddl($conn, $query)
+{
+  global $verbose, $for_real;
+
+  if ($verbose) {
+    echo "Query: $query\n\n";
+  }
+  if ($for_real) {
+    mysqli_query($conn, $query) or die(mysqli_error($conn));
+  }
+}
+
